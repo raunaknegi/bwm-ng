5328238af63a7a9454ae9df0c215d5a32564907c
raunak <negiraunak424@gmail.com>
2019-10-08 00:09:57 +0530

    Adds header compenent and scss styles

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -6,5 +6,8 @@ import { Component } from '@angular/core';
   styleUrls: ['./app.component.css']
 })
 export class AppComponent {
-  title = 'app';
+  componentTitle="hello world from component.ts!"; 
+  clickHandler(){
+    alert("you clicked the buttton");
+  }
 }
