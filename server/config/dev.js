0d8e2821db3d618478cb719e444d97d845a7a228
raunaknegi <48157636+raunaknegi@users.noreply.github.com>
2020-01-15 21:27:01 +0530

    Update dev.js

--- server/config/dev.js
+++ server/config/dev.js
@@ -1,6 +1,1 @@
-module.exports={
-    BWM_URI:'mongodb+srv://test:testtest@cluster0-p3eob.mongodb.net/test?retryWrites=true&w=majority',
-    SECRET:'secret',
-    aws_ACCESS_KEY:'AKIAITWFB5THBWNMBMPQ',
-    aws_SECRET_ACCESS_KEY:'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'
-}
+
