b4d943d0015ec5380885dfe633c5df5de69c36ba
raunaknegi <48157636+raunaknegi@users.noreply.github.com>
2020-01-26 23:26:40 +0530

    Update dev.js

--- server/config/dev.js
+++ server/config/dev.js
@@ -1,5 +1,3 @@
 module.exports={
-    BWM_URI:'mongodb+srv://test:testtest@cluster0-p3eob.mongodb.net/test?retryWrites=true&w=majority',
-    SECRET:'secret',
-    STRIPE_SK:'sk_test_iJGEnVXTYBFIcDByysxS7L0300PNZC6vzn'
+ 
 }
